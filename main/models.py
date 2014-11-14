1f6408180ea7253af1572b515fa422dc436eaf7f
Alex Dobrushskiy <Alexey.Dobrushskiy@artezio.com>
2014-11-14 13:54:05 +0400

    Breake app a little:)

--- main/models.py
+++ main/models.py
@@ -2,7 +2,3 @@ from django.db import models
 
 
 # Create your models here.
-class Post(models.Model):
-    header = models.CharField(max_length=255)
-    text = models.TextField()
-    when = models.DateTimeField(auto_now=True)
--- main/views.py
+++ main/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
-from main.models import Post
+# from main.models import Post
 # Create your views here.
 
 
 class Posts(View):
     def get(self, request):
-        posts = Post.objects.all()
-        return HttpResponse(render(request, 'posts.html', {'posts': posts}))
+        return HttpResponse(render(request, 'posts.html', {}))
