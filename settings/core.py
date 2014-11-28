29919554c63bc03f7eed5aea8bee298963862cb4
Alex Dobrushskiy <Alexey.Dobrushskiy@artezio.com>
2014-11-28 13:03:10 +0300

    Heroku settings

--- settings/core.py
+++ settings/core.py
@@ -26,6 +26,12 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Application definition
 
