b823fbdb7ce6bc66f92229fd2bc820feb1e2895d
unknown <adobrushskiy@adobrushskiy.ARTGROUP.local>
2014-11-28 12:50:50 +0300

    Revert "Breake app a little:)"
    This reverts commit 1f6408180ea7253af1572b515fa422dc436eaf7f.

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-
+from main.models import Post
 # Register your models here.
 
+admin.site.register(Post)
--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
-# from main.models import Post
+from main.models import Post
 # Create your views here.
 
 
 class Posts(View):
     def get(self, request):
-        return HttpResponse(render(request, 'posts.html', {}))
+        posts = Post.objects.all()
+        return HttpResponse(render(request, 'posts.html', {'posts': posts}))
